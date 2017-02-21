93a3a1b84570afe8e9f6a04b833134ed0214802e
Allen Tung <Snowrries@users.noreply.github.com>
2017-02-21 15:24:18 -0500

    reordered for row major acccesses
    adding restrict keyword

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -12,7 +12,7 @@ MKLROOT = /opt/intel/composer_xe_2013.1.117/mkl
 LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKLROOT)/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm
 
 */
-
+#include "immintrin.h"
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
@@ -27,100 +27,105 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 static void do_block(int lda, int M, int N, int K, double* A, double* B, double* C)
 {
 	double b1, b2, b3, b4, b5, b6, b7, b8;
+	double c1, c2, c3, c4, c5, c6, c7, c8;
 	int i, j, k;
 	//Expand j, k, then i. (Maybe have to alter, if this is column major? )
 	for (i = 0; i < M; ++i) {
 
 		for (k = 0; k < (K - 7); k += 8) {
 
-			b1 = B[ lda*i + k];
-			b2 = B[ lda*i + k + 1];
-			b3 = B[ lda*i + k + 2];
-			b4 = B[ lda*i + k + 3];
-			b5 = B[ lda*i + k + 4];
-			b6 = B[ lda*i + k + 5];
-			b7 = B[ lda*i + k + 6];
-			b8 = B[ lda*i + k + 7];
+			restrict b1 = B[ lda*i + k];
+			restrict b2 = B[ lda*i + k + 1];
+			restrict b3 = B[ lda*i + k + 2];
+			restrict b4 = B[ lda*i + k + 3];
+			restrict b5 = B[ lda*i + k + 4];
+			restrict b6 = B[ lda*i + k + 5];
+			restrict b7 = B[ lda*i + k + 6];
+			restrict b8 = B[ lda*i + k + 7];
 
 			for (j = 0; j < (N - 7); j += 8) {
 
-				C[lda*i + j] += A[ lda*k+j] * b1;
-				C[lda*i + j] += A[ lda*(k + 1)+j] * b2;
-				C[lda*i + j] += A[ lda*(k + 2)+j] * b3;
-				C[lda*i + j] += A[ lda*(k + 3)+j] * b4;
-				C[lda*i + j] += A[ lda*(k + 4)+j] * b5;
-				C[lda*i + j] += A[ lda*(k + 5)+j] * b6;
-				C[lda*i + j] += A[ lda*(k + 6)+j] * b7;
-				C[lda*i + j] += A[ lda*(k + 7)+j] * b8;
+				restrict c1 = C[lda*i + j];
+				restrict c2 = C[lda*i + (j+1)];
+				restrict c3 = C[lda*i + (j+2)];
+				restrict c4 = C[lda*i + (j+3)];
+				restrict c5 = C[lda*i + (j+4)];
+				restrict c6 = C[lda*i + (j+5)];
+				restrict c7 = C[lda*i + (j+6)];
+				restrict c8 = C[lda*i + (j+7)];
 
-				C[lda*i + (j+1)] += A[ lda*k+(j+1)] * b1;
-				C[lda*i + (j+1)] += A[ lda*(k + 1)+(j+1)] * b2;
-				C[lda*i + (j+1)] += A[ lda*(k + 2)+(j+1)] * b3;
-				C[lda*i + (j+1)] += A[ lda*(k + 3)+(j+1)] * b4;
-				C[lda*i + (j+1)] += A[ lda*(k + 4)+(j+1)] * b5;
-				C[lda*i + (j+1)] += A[ lda*(k + 5)+(j+1)] * b6;
-				C[lda*i + (j+1)] += A[ lda*(k + 6)+(j+1)] * b7;
-				C[lda*i + (j+1)] += A[ lda*(k + 7)+(j+1)] * b8;
-				
 
-				C[lda*i + (j+2)] += A[ lda*k+(j+2)] * b1;
-				C[lda*i + (j+2)] += A[ lda*(k + 1)+(j+2)] * b2;
-				C[lda*i + (j+2)] += A[ lda*(k + 2)+(j+2)] * b3;
-				C[lda*i + (j+2)] += A[ lda*(k + 3)+(j+2)] * b4;
-				C[lda*i + (j+2)] += A[ lda*(k + 4)+(j+2)] * b5;
-				C[lda*i + (j+2)] += A[ lda*(k + 5)+(j+2)] * b6;
-				C[lda*i + (j+2)] += A[ lda*(k + 6)+(j+2)] * b7;
-				C[lda*i + (j+2)] += A[ lda*(k + 7)+(j+2)] * b8;
-				
+				c1 += restrict A[ lda*k+j] * b1;
+				c2 += restrict A[ lda*k+(j+1)] * b1;
+				c3 += restrict A[ lda*k+(j+2)] * b1;
+				c4 += restrict A[ lda*k+(j+3)] * b1;
+				c5 += restrict A[ lda*k+(j+4)] * b1;
+				c6 += restrict A[ lda*k+(j+5)] * b1;
+				c7 += restrict A[ lda*k+(j+6)] * b1;
+				c8 += restrict A[ lda*k+(j+7)] * b1;
 
-				C[lda*i + (j+3)] += A[ lda*k+(j+3)] * b1;
-				C[lda*i + (j+3)] += A[ lda*(k + 1)+(j+3)] * b2;
-				C[lda*i + (j+3)] += A[ lda*(k + 2)+(j+3)] * b3;
-				C[lda*i + (j+3)] += A[ lda*(k + 3)+(j+3)] * b4;
-				C[lda*i + (j+3)] += A[ lda*(k + 4)+(j+3)] * b5;
-				C[lda*i + (j+3)] += A[ lda*(k + 5)+(j+3)] * b6;
-				C[lda*i + (j+3)] += A[ lda*(k + 6)+(j+3)] * b7;
-				C[lda*i + (j+3)] += A[ lda*(k + 7)+(j+3)] * b8;
-				
+				c1 += restrict A[ lda*(k + 1)+j] * b2;
+				c2 += restrict A[ lda*(k + 1)+(j+1)] * b2;
+				c3 += restrict A[ lda*(k + 1)+(j+2)] * b2;
+				c4 += restrict A[ lda*(k + 1)+(j+3)] * b2;
+				c5 += restrict A[ lda*(k + 1)+(j+4)] * b2;
+				c6 += restrict A[ lda*(k + 1)+(j+5)] * b2;
+				c7 += restrict A[ lda*(k + 1)+(j+6)] * b2;
+				c8 += restrict A[ lda*(k + 1)+(j+7)] * b2;
 
-				C[lda*i + (j+4)] += A[ lda*k+(j+4)] * b1;
-				C[lda*i + (j+4)] += A[ lda*(k + 1)+(j+4)] * b2;
-				C[lda*i + (j+4)] += A[ lda*(k + 2)+(j+4)] * b3;
-				C[lda*i + (j+4)] += A[ lda*(k + 3)+(j+4)] * b4;
-				C[lda*i + (j+4)] += A[ lda*(k + 4)+(j+4)] * b5;
-				C[lda*i + (j+4)] += A[ lda*(k + 5)+(j+4)] * b6;
-				C[lda*i + (j+4)] += A[ lda*(k + 6)+(j+4)] * b7;
-				C[lda*i + (j+4)] += A[ lda*(k + 7)+(j+4)] * b8;
-				
+				c1 += restrict A[ lda*(k + 2)+j] * b3;
+				c2 += restrict A[ lda*(k + 2)+(j+1)] * b3;
+				c3 += restrict A[ lda*(k + 2)+(j+2)] * b3;
+				c4 += restrict A[ lda*(k + 2)+(j+3)] * b3;
+				c5 += restrict A[ lda*(k + 2)+(j+4)] * b3;
+				c6 += restrict A[ lda*(k + 2)+(j+5)] * b3;
+				c7 += restrict A[ lda*(k + 2)+(j+6)] * b3;
+				c8 += restrict A[ lda*(k + 2)+(j+7)] * b3;
 
-				C[lda*i + (j+5)] += A[ lda*k+(j+5)] * b1;
-				C[lda*i + (j+5)] += A[ lda*(k + 1)+(j+5)] * b2;
-				C[lda*i + (j+5)] += A[ lda*(k + 2)+(j+5)] * b3;
-				C[lda*i + (j+5)] += A[ lda*(k + 3)+(j+5)] * b4;
-				C[lda*i + (j+5)] += A[ lda*(k + 4)+(j+5)] * b5;
-				C[lda*i + (j+5)] += A[ lda*(k + 5)+(j+5)] * b6;
-				C[lda*i + (j+5)] += A[ lda*(k + 6)+(j+5)] * b7;
-				C[lda*i + (j+5)] += A[ lda*(k + 7)+(j+5)] * b8;
-				
+				c1 += restrict A[ lda*(k + 3)+j] * b4;
+				c2 += restrict A[ lda*(k + 3)+(j+1)] * b4;
+				c3 += restrict A[ lda*(k + 3)+(j+2)] * b4;
+				c4 += restrict A[ lda*(k + 3)+(j+3)] * b4;
+				c5 += restrict A[ lda*(k + 3)+(j+4)] * b4;
+				c6 += restrict A[ lda*(k + 3)+(j+5)] * b4;
+				c7 += restrict A[ lda*(k + 3)+(j+6)] * b4;
+				c8 += restrict A[ lda*(k + 3)+(j+7)] * b4;
 
-				C[lda*i + (j+6)] += A[ lda*k+(j+6)] * b1;
-				C[lda*i + (j+6)] += A[ lda*(k + 1)+(j+6)] * b2;
-				C[lda*i + (j+6)] += A[ lda*(k + 2)+(j+6)] * b3;
-				C[lda*i + (j+6)] += A[ lda*(k + 3)+(j+6)] * b4;
-				C[lda*i + (j+6)] += A[ lda*(k + 4)+(j+6)] * b5;
-				C[lda*i + (j+6)] += A[ lda*(k + 5)+(j+6)] * b6;
-				C[lda*i + (j+6)] += A[ lda*(k + 6)+(j+6)] * b7;
-				C[lda*i + (j+6)] += A[ lda*(k + 7)+(j+6)] * b8;
-				
+				c1 += restrict A[ lda*(k + 4)+j] * b5;
+				c2 += restrict A[ lda*(k + 4)+(j+1)] * b5;
+				c3 += restrict A[ lda*(k + 4)+(j+2)] * b5;
+				c4 += restrict A[ lda*(k + 4)+(j+3)] * b5;
+				c5 += restrict A[ lda*(k + 4)+(j+4)] * b5;
+				c6 += restrict A[ lda*(k + 4)+(j+5)] * b5;
+				c7 += restrict A[ lda*(k + 4)+(j+6)] * b5;
+				c8 += restrict A[ lda*(k + 4)+(j+7)] * b5;
 
-				C[lda*i + (j+7)] += A[ lda*k+(j+7)] * b1;
-				C[lda*i + (j+7)] += A[ lda*(k + 1)+(j+7)] * b2;
-				C[lda*i + (j+7)] += A[ lda*(k + 2)+(j+7)] * b3;
-				C[lda*i + (j+7)] += A[ lda*(k + 3)+(j+7)] * b4;
-				C[lda*i + (j+7)] += A[ lda*(k + 4)+(j+7)] * b5;
-				C[lda*i + (j+7)] += A[ lda*(k + 5)+(j+7)] * b6;
-				C[lda*i + (j+7)] += A[ lda*(k + 6)+(j+7)] * b7;
-				C[lda*i + (j+7)] += A[ lda*(k + 7)+(j+7)] * b8;
+				c1 += restrict A[ lda*(k + 5)+j] * b6;
+				c2 += restrict A[ lda*(k + 5)+(j+1)] * b6;
+				c3 += restrict A[ lda*(k + 5)+(j+2)] * b6;
+				c4 += restrict A[ lda*(k + 5)+(j+3)] * b6;
+				c5 += restrict A[ lda*(k + 5)+(j+4)] * b6;
+				c6 += restrict A[ lda*(k + 5)+(j+5)] * b6;
+				c7 += restrict A[ lda*(k + 5)+(j+6)] * b6;
+				c8 += restrict A[ lda*(k + 5)+(j+7)] * b6;
+
+				c1 += restrict A[ lda*(k + 6)+j] * b7;
+				c2 += restrict A[ lda*(k + 6)+(j+1)] * b7;
+				c3 += restrict A[ lda*(k + 6)+(j+2)] * b7;
+				c4 += restrict A[ lda*(k + 6)+(j+3)] * b7;
+				c5 += restrict A[ lda*(k + 6)+(j+4)] * b7;
+				c6 += restrict A[ lda*(k + 6)+(j+5)] * b7;
+				c7 += restrict A[ lda*(k + 6)+(j+6)] * b7;
+				c8 += restrict A[ lda*(k + 6)+(j+7)] * b7;
+
+				c1 += restrict A[ lda*(k + 7)+j] * b8;
+				c2 += restrict A[ lda*(k + 7)+(j+1)] * b8;
+				c3 += restrict A[ lda*(k + 7)+(j+2)] * b8;
+				c4 += restrict A[ lda*(k + 7)+(j+3)] * b8;
+				c5 += restrict A[ lda*(k + 7)+(j+4)] * b8;
+				c6 += restrict A[ lda*(k + 7)+(j+5)] * b8;
+				c7 += restrict A[ lda*(k + 7)+(j+6)] * b8;
+				c8 += restrict A[ lda*(k + 7)+(j+7)] * b8;
 
 			}	
 			if(N % 8){
