5bff6d686ae913e7b431134206cf84908a0fa22e
Allen Tung <Snowrries@users.noreply.github.com>
2017-02-22 03:06:06 -0500

    rerolled k

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -33,61 +33,21 @@ static void do_block(int lda, int M, int N, int K, double* A, double* B, double*
 	//Expand j, k, then i. (Maybe have to alter, if this is column major? )
 	for (j = 0; j < N; ++j) {
 
-		for (k = 0; k < (K - 3); k += 4) {
+		for (k = 0; k < K; ++k) {
 			temp = lda*j + k; 
 			b1 = _mm256_broadcast_sd (&B[ temp ]);
-			b2 = _mm256_broadcast_sd (&B[ temp + 1]);
-			b3 = _mm256_broadcast_sd (&B[ temp + 2]);
-			b4 = _mm256_broadcast_sd (&B[ temp + 3]);
 			for (i = 0; i < (M - 3); i += 4) {
 				c = _mm256_loadu_pd(&C[lda*j + i]);
-				temp = lda*k+i;
-				temp1 = _mm256_mul_pd(_mm256_loadu_pd(&A[temp]),b1);
-				temp2 = _mm256_mul_pd(_mm256_loadu_pd(&A[temp += lda]), b2);
-				temp3 = _mm256_mul_pd(_mm256_loadu_pd(&A[temp += lda]), b3);
-				temp4 = _mm256_mul_pd(_mm256_loadu_pd(&A[temp += lda]), b4);
-
-				temp5 = _mm256_add_pd(temp1, temp2);
-
-				temp6 = _mm256_add_pd(temp3, temp4);
-
-				c = _mm256_add_pd(c, temp5);
-
-				c = _mm256_add_pd(c, temp6);
-
+				c = _mm256_add_pd(c, _mm256_mul_pd(_mm256_loadu_pd(&A[lda*k+i]),b1));
 				_mm256_storeu_pd(&C[lda*j + i],c);
 
 			}	
 			if(M % 4){
-				temp = j*lda+k
-				bb1 = B[temp];
-				bb2 = B[temp+1];
-				bb3 = B[temp+2];
-				bb4 = B[temp+3];
+				bb1 = B[j*lda+k];
 				for (; i < M; ++i) {
-					temp = lda*k+i;
-					C[lda*j + i] += A[temp] * bb1;
-					C[lda*j + i] += A[temp+=lda] * bb2;
-					C[lda*j + i] += A[temp+=lda] * bb3;
-					C[lda*j + i] += A[temp+=lda] * bb4;
+					C[lda*j + i] += A[lda*k+i] * bb1;
 				}
 			}
-		}
-		if (K % 4) {
-			do {
-				b1 = _mm256_broadcast_sd (&B[ lda*j + k]);
-				for (i = 0; i < (M - 3); i += 4) {
-					c = _mm256_loadu_pd(&C[lda*j + i]);
-					c = _mm256_add_pd(c, _mm256_mul_pd(_mm256_loadu_pd(&A[lda*k + i]), b1));
-					_mm256_storeu_pd(&C[lda*j + i],c);
-				}
-				if(M % 4){
-					bb1 = B[j*lda + k];
-					for (; i < M; ++i) {				
-						C[lda*j + i] += A[lda*k + i] * bb1;
-					}
-				}
-			} while (++k < K);
 		}
 	}
 }
