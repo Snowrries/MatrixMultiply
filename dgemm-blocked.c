abef0a7ad7af037f787556e0682193cf7a014699
Allen Tung <Snowrries@users.noreply.github.com>
2017-02-21 20:56:36 -0500

    blocked
    bugfixes

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -12,7 +12,7 @@ MKLROOT = /opt/intel/composer_xe_2013.1.117/mkl
 LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKLROOT)/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm
 
 */
-
+#include "immintrin.h"
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
@@ -26,122 +26,71 @@ const char* dgemm_desc = "Simple blocked dgemm.";
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block(int lda, int M, int N, int K, double* A, double* B, double* C)
 {
-	double b1, b2, b3, b4, b5, b6, b7, b8;
+	double b1;
+	__m256d b, c;
 	int i, j, k;
 	//Expand j, k, then i. (Maybe have to alter, if this is column major? )
-	for (i = 0; i < M; ++i) {
+	for (j = 0; j < N; ++j) {
 
-		for (k = 0; k < (K - 7); k += 8) {
+		for (k = 0; k < (K - 3); k += 4) {
 
-			b1 = B[ lda*i + k];
-			b2 = B[ lda*i + k + 1];
-			b3 = B[ lda*i + k + 2];
-			b4 = B[ lda*i + k + 3];
-			b5 = B[ lda*i + k + 4];
-			b6 = B[ lda*i + k + 5];
-			b7 = B[ lda*i + k + 6];
-			b8 = B[ lda*i + k + 7];
+			b = _mm256_load_pd (B[ lda*i + k]);
 
-			for (j = 0; j < (N - 7); j += 8) {
 
-				C[lda*i + j] += A[ lda*k+j] * b1;
-				C[lda*i + j] += A[ lda*(k + 1)+j] * b2;
-				C[lda*i + j] += A[ lda*(k + 2)+j] * b3;
-				C[lda*i + j] += A[ lda*(k + 3)+j] * b4;
-				C[lda*i + j] += A[ lda*(k + 4)+j] * b5;
-				C[lda*i + j] += A[ lda*(k + 5)+j] * b6;
-				C[lda*i + j] += A[ lda*(k + 6)+j] * b7;
-				C[lda*i + j] += A[ lda*(k + 7)+j] * b8;
+			for (i = 0; i < (M - 3); i += 4) {
+				c = _mm256_load_pd(&C[lda*j + i]);
+				_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*k + i]),b),c));
+				_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+1) + i]),b),c));
+				_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+2) + i]),b),c));
+				_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+3) + i]),b),c));
 
-				C[lda*i + (j+1)] += A[ lda*k+(j+1)] * b1;
-				C[lda*i + (j+1)] += A[ lda*(k + 1)+(j+1)] * b2;
-				C[lda*i + (j+1)] += A[ lda*(k + 2)+(j+1)] * b3;
-				C[lda*i + (j+1)] += A[ lda*(k + 3)+(j+1)] * b4;
-				C[lda*i + (j+1)] += A[ lda*(k + 4)+(j+1)] * b5;
-				C[lda*i + (j+1)] += A[ lda*(k + 5)+(j+1)] * b6;
-				C[lda*i + (j+1)] += A[ lda*(k + 6)+(j+1)] * b7;
-				C[lda*i + (j+1)] += A[ lda*(k + 7)+(j+1)] * b8;
-				
+				c = _mm256_load_pd(&C[lda*j + (i+1)]);
+				_mm256_store_pd(&C[lda*j + (i+1)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*k + (i+1)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+1)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+1) + (i+1)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+1)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+2) + (i+1)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+1)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+3) + (i+1)]),b),c));
 
-				C[lda*i + (j+2)] += A[ lda*k+(j+2)] * b1;
-				C[lda*i + (j+2)] += A[ lda*(k + 1)+(j+2)] * b2;
-				C[lda*i + (j+2)] += A[ lda*(k + 2)+(j+2)] * b3;
-				C[lda*i + (j+2)] += A[ lda*(k + 3)+(j+2)] * b4;
-				C[lda*i + (j+2)] += A[ lda*(k + 4)+(j+2)] * b5;
-				C[lda*i + (j+2)] += A[ lda*(k + 5)+(j+2)] * b6;
-				C[lda*i + (j+2)] += A[ lda*(k + 6)+(j+2)] * b7;
-				C[lda*i + (j+2)] += A[ lda*(k + 7)+(j+2)] * b8;
-				
 
-				C[lda*i + (j+3)] += A[ lda*k+(j+3)] * b1;
-				C[lda*i + (j+3)] += A[ lda*(k + 1)+(j+3)] * b2;
-				C[lda*i + (j+3)] += A[ lda*(k + 2)+(j+3)] * b3;
-				C[lda*i + (j+3)] += A[ lda*(k + 3)+(j+3)] * b4;
-				C[lda*i + (j+3)] += A[ lda*(k + 4)+(j+3)] * b5;
-				C[lda*i + (j+3)] += A[ lda*(k + 5)+(j+3)] * b6;
-				C[lda*i + (j+3)] += A[ lda*(k + 6)+(j+3)] * b7;
-				C[lda*i + (j+3)] += A[ lda*(k + 7)+(j+3)] * b8;
-				
+				c = _mm256_load_pd(&C[lda*j + (i+2)]);
+				_mm256_store_pd(&C[lda*j + (i+2)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*k + (i+2)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+2)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+1) + (i+2)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+2)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+2) + (i+2)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+2)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+3) + (i+2)]),b),c));
 
-				C[lda*i + (j+4)] += A[ lda*k+(j+4)] * b1;
-				C[lda*i + (j+4)] += A[ lda*(k + 1)+(j+4)] * b2;
-				C[lda*i + (j+4)] += A[ lda*(k + 2)+(j+4)] * b3;
-				C[lda*i + (j+4)] += A[ lda*(k + 3)+(j+4)] * b4;
-				C[lda*i + (j+4)] += A[ lda*(k + 4)+(j+4)] * b5;
-				C[lda*i + (j+4)] += A[ lda*(k + 5)+(j+4)] * b6;
-				C[lda*i + (j+4)] += A[ lda*(k + 6)+(j+4)] * b7;
-				C[lda*i + (j+4)] += A[ lda*(k + 7)+(j+4)] * b8;
-				
 
-				C[lda*i + (j+5)] += A[ lda*k+(j+5)] * b1;
-				C[lda*i + (j+5)] += A[ lda*(k + 1)+(j+5)] * b2;
-				C[lda*i + (j+5)] += A[ lda*(k + 2)+(j+5)] * b3;
-				C[lda*i + (j+5)] += A[ lda*(k + 3)+(j+5)] * b4;
-				C[lda*i + (j+5)] += A[ lda*(k + 4)+(j+5)] * b5;
-				C[lda*i + (j+5)] += A[ lda*(k + 5)+(j+5)] * b6;
-				C[lda*i + (j+5)] += A[ lda*(k + 6)+(j+5)] * b7;
-				C[lda*i + (j+5)] += A[ lda*(k + 7)+(j+5)] * b8;
-				
+				c = _mm256_load_pd(&C[lda*j + (i+3)]);
+				_mm256_store_pd(&C[lda*j + (i+3)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*k + (i+3)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+3)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+1) + (i+3)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+3)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+2) + (i+3)]),b),c));
+				_mm256_store_pd(&C[lda*j + (i+3)],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+3) + (i+3)]),b),c));
 
-				C[lda*i + (j+6)] += A[ lda*k+(j+6)] * b1;
-				C[lda*i + (j+6)] += A[ lda*(k + 1)+(j+6)] * b2;
-				C[lda*i + (j+6)] += A[ lda*(k + 2)+(j+6)] * b3;
-				C[lda*i + (j+6)] += A[ lda*(k + 3)+(j+6)] * b4;
-				C[lda*i + (j+6)] += A[ lda*(k + 4)+(j+6)] * b5;
-				C[lda*i + (j+6)] += A[ lda*(k + 5)+(j+6)] * b6;
-				C[lda*i + (j+6)] += A[ lda*(k + 6)+(j+6)] * b7;
-				C[lda*i + (j+6)] += A[ lda*(k + 7)+(j+6)] * b8;
-				
 
-				C[lda*i + (j+7)] += A[ lda*k+(j+7)] * b1;
-				C[lda*i + (j+7)] += A[ lda*(k + 1)+(j+7)] * b2;
-				C[lda*i + (j+7)] += A[ lda*(k + 2)+(j+7)] * b3;
-				C[lda*i + (j+7)] += A[ lda*(k + 3)+(j+7)] * b4;
-				C[lda*i + (j+7)] += A[ lda*(k + 4)+(j+7)] * b5;
-				C[lda*i + (j+7)] += A[ lda*(k + 5)+(j+7)] * b6;
-				C[lda*i + (j+7)] += A[ lda*(k + 6)+(j+7)] * b7;
-				C[lda*i + (j+7)] += A[ lda*(k + 7)+(j+7)] * b8;
 
 			}	
-			if(N % 8){
-				for (; j < N; j ++) {
-					C[lda*i + j] += A[ lda*k+j] * b1;
-					C[lda*i + j] += A[ lda*(k + 1)+j] * b2;
-					C[lda*i + j] += A[ lda*(k + 2)+j] * b3;
-					C[lda*i + j] += A[ lda*(k + 3)+j] * b4;
-					C[lda*i + j] += A[ lda*(k + 4)+j] * b5;
-					C[lda*i + j] += A[ lda*(k + 5)+j] * b6;
-					C[lda*i + j] += A[ lda*(k + 6)+j] * b7;
-					C[lda*i + j] += A[ lda*(k + 7)+j] * b8;
+			if(M % 4){
+				b = _mm256_load_pd (&B[ lda*i + k]);
+				for (; i < M; i ++) {				
+					c = _mm256_load_pd(C[lda*j + i]);
+					_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*k + i]),b),c));
+					_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+1) + i]),b),c));
+					_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+2) + i]),b),c));
+					_mm256_store_pd(&C[lda*j + i],_mm256_add_pd(_mm256_mul_pd(_mm256_load_pd(&A[lda*(k+3) + i]),b),c));
 				}
 			}
 		}
-		if (K % 8) {
+		if (K % 4) {
 			do {
 				b1 = B[j*lda + k];
-				for (j = 0; j < N; ++j) {
-
-					C[lda*i + j] += A[k*lda + j] * b1;
+				for (i = 0; i < (M - 3); i += 4) {
+					C[lda*j + i] = A[lda*k + i] * b1;
+					C[lda*j + (i+1)] = A[lda*k + (i+1)] * b1;
+					C[lda*j + (i+2)] = A[lda*k + (i+2)] * b1;
+					C[lda*j + (i+3)] = A[lda*k + (i+3)] * b1;
+				}
+				if(M % 4){
+					for (; i < M; i ++) {				
+						C[lda*j + i] = A[lda*k + i] * b1;
+					}
 				}
 			} while (++k < K);
 		}
